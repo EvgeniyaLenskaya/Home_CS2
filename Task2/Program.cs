cae7e7f70a9fb68375702ede7b56b5b988e9b1c5
EvgeniyaLenskaya <janie2010@mail.ru>
2023-03-06 13:42:37 +0800

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number / 100 % 10;
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    if (number >= 10000 && number < 100000)
+    {
+        Console.WriteLine ($"{number} -> {result}");
+    }
+    else
+    {
+        Console.WriteLine ("Вы ввели НЕ пятизначное число.");
+    }
+}        
