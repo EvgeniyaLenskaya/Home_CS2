d6150e9884dbf344d93ae9534915c5dc25e1ccf4
EvgeniyaLenskaya <janie2010@mail.ru>
2023-03-06 14:01:57 +0800

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write ("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine ());
+
+if (day >= 1 && day < 6)
+{
+    Console.WriteLine ("Идём на работу(");
+}
+else
+{
+    if (day >=6 && day < 8)
+    {
+        Console.WriteLine ("Ура! Выходной!");
+    }
+    else
+    {
+        Console.WriteLine ("Это не день недели.");
+    }
+}
